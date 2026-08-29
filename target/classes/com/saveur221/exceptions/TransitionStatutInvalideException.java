6a8b1fe3fb77d0446cbd7e08534983c3e6343c53
crybaby05 <benthiam750@gmail.com>
2026-08-29 12:17:24 +0000

    Ajouter la classe TransitionStatutInvalideException pour gérer les exceptions liées à un statut invalide

---
+++ target/classes/com/saveur221/exceptions/TransitionStatutInvalideException.java
@@ -0,0 +1,7 @@
+package com.saveur221.exceptions;
+
+public class TransitionStatutInvalideException extends RuntimeException {
+    public TransitionStatutInvalideException(String message) {
+        super(message);
+    }
+}
