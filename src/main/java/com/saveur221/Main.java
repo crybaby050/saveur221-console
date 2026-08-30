78d761dc598a6ee949945a19158d806a521a798a
crybaby05 <benthiam750@gmail.com>
2026-08-30 01:09:15 +0000

    Ajout de la gestion de l'écran d'accueil avec options de connexion dans la classe Main.

--- src/main/java/com/saveur221/Main.java
+++ src/main/java/com/saveur221/Main.java
@@ -2,6 +2,7 @@ package com.saveur221;
 
 import com.saveur221.config.Container;
 import com.saveur221.entities.Utilisateur;
+import com.saveur221.views.AccueilView;
 import com.saveur221.views.LoginView;
 import com.saveur221.views.Router;
 
@@ -12,6 +13,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Container container = new Container();
+
+        AccueilView accueilView = new AccueilView(scanner);
+        accueilView.demarrer();
+
+        if (!accueilView.aChoisiDeSeConnecter()) {
+            System.out.println("Au revoir !");
+            return;
+        }
 
         LoginView loginView = new LoginView(container.getAuthService(), scanner);
         Utilisateur utilisateurConnecte = loginView.demarrerConnexion();
